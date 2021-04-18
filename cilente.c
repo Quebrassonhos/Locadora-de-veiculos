bf1b7913aff88bb29c421815cfce6bb2411bf5f1
JelsonMatheus <58706567+JelsonMatheus@users.noreply.github.com>
2021-04-18 20:21:22 -0300

    corrige indentaçao

--- cilente.c
+++ cilente.c
@@ -4,15 +4,15 @@
 
 void cadastrarCliente(Cliente* clientes; int* indexCliente) {
 	
-	printf("Digite seu nome: ");
-	scanf("%[^\n]s%*c", &clientes[indexCliente]->nome);
-	printf("Digite seu cpf: ");
-	scanf("%d", &clientes[indexCliente]->cpf);
-	printf("Digite o n˙mero de telefone: "); //nao esquecer de colocar a funcao que habilita acentuacao.
-	scanf("%d", &clientes[indexCliente]->telefone);
+    printf("Digite seu nome: ");
+    scanf("%[^\n]s%*c", &clientes[indexCliente].nome);
+
+    Printf("Digite seu cpf: ");
+    scanf("%d", &clientes[indexCliente].cpf);
+
+    printf("Digite o n√∫mero de telefone: "); //nao esquecer de colocar a funcao que habilita acentuacao.
+    scanf("%d", &clientes[indexCliente].telefone);
     indexCliente = indexCliente + 1;
-    
-    
 }
 
 
