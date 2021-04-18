a92742ef22bc8cae055269701601863811fbd28d
JelsonMatheus <>
2021-04-18 13:18:01 +0000

    Adicionado as declarações de struct

---
+++ entidades/carro.h
@@ -0,0 +1,13 @@
+#ifndef CARRO_H   // Evita as inclusões cíclicas
+#define CARRO_H
+
+//Declaração de struct
+
+typedef struct carro {
+    char modelo[128];
+    char placa[8];
+} Carro;
+
+// Declaração das funções aqui
+
+#endif
---
+++ entidades/cliente.h
@@ -0,0 +1,14 @@
+#ifndef CLIENTE_H   // Evita as inclusões cíclicas
+#define CLIENTE_H
+
+// Declaração de struct
+
+typedef struct cliente {
+    char nome[128];
+    char cpf[10];
+    int telefone;
+} Cliente;
+
+// Declaração das funções aqui
+
+#endif
