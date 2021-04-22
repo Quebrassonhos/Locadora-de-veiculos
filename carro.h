2907e1d1a665811da338ae892b95c0ba2a0d9758
julianaasued <79534351+julianaasued@users.noreply.github.com>
2021-04-21 21:04:39 -0300

    Update carro.h

--- carro.h
+++ carro.h
@@ -1,14 +1,14 @@
-#ifndef CARRO_H   // Evita as inclusões cíclicas
+#ifndef CARRO_H   // Evita as inclusÃµes cÃ­clicas
 #define CARRO_H
 
-//Declaração de struct
+//DeclaraÃ§Ã£o de struct
 
 typedef struct carro {
     char modelo[128];
     char placa[8];
 } Carro;
 
-// Declaração das funções aqui
+// DeclaraÃ§Ã£o das funÃ§Ãµes aqui
 
 Carro criarCarro();
 
@@ -19,5 +19,7 @@ void excluirCarro(Carro *carros, int *indexCarro, int pos);
 void printCarro(Carro carro);     
 
 int buscarCarro(Carro *carros, int indexCarro, char placa[8]);
+
+void validaPlaca(Carro *carros, int *indexCarro, int pos);
 
 #endif 
